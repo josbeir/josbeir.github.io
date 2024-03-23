760b6febc2304bab1aeb809cf401aad5dffd6580
Jasper Smet <josbeir@gmail.com>
2024-03-23 14:11:30 +0100

    Initial hugo commit

---
+++ content/home/contact/_index.md
@@ -0,0 +1,8 @@
+---
+title: "Let's talk"
+template: contact
+weight: 0
+---
+## Let's talk
+
+Feel free to reach out to me directly via the contact form below. I'm eager to hear about your project ideas and discuss how we can collaborate to bring them to life. Let's connect and make something awesome together!
---
+++ themes/josbeir/assets/js/main.js
@@ -0,0 +1,17 @@
+// Make sure the back button works.
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.querySelector('main');
+
+    window.addEventListener('hashchange', () => {
+        let selector = window.location.hash;
+
+        if (!selector) {
+            selector = 'section:first-child';
+        }
+
+        let el = container.querySelector(selector);
+        if (el) {
+            el.scrollIntoView();
+        }
+    });
+});
